343a6e3da8f169dc213c749c994addf8e9957859
goryugo@iMac <goryugo33@gmail.com>
2009-02-22 18:30:03 +0900

    add GISTY_DIR

--- .emacs.el
+++ .emacs.el
@@ -7,9 +7,12 @@
        )
        load-path))
 ;;migemo 
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-;;あとで
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+(load "migemo.el")
+(setq migemo-command "cmigemo")
+(setq migemo-options '("-q" "--emacs" "-i" "¥a"))
+(setq migemo-dictionary "/usr/local/share/migemo/euc-jp/migemo-dict")
+(setq migemo-user-dictionary nil)
+(setq migemo-regex-dictionary nil)
 ;;(load "migemo.el")
 ;;(setq migemo-use-pattern-alist t)
 ;;(setq migemo-use-frequent-pattern-alist t)
